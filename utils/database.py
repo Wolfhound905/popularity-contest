a2eb6f848fbb990b9e485b04af0598d1166229b0
Wolfhound905 <aiden8green@gmail.com>
2021-10-29 04:58:11 +0000

    better database

--- utils/database.py
+++ utils/database.py
@@ -3,6 +3,7 @@ from typing import Any, Union
 from pymysql.connections import Connection
 from pymysql.cursors import Cursor
 from utils.models import Star
+from dis_snek.client import Snake
 
 
 class Database:
@@ -16,7 +17,8 @@ class Database:
             (guild_id, channel_id, star_count),
         )
         return
-    def min_stars(self, guild_id:int) -> Union[int, None]:
+
+    def min_stars(self, guild_id: int) -> Union[int, None]:
         self.__db.execute(
             "SELECT min_star_count FROM configuration WHERE guild_id = %s;", (guild_id,)
         )
@@ -25,43 +27,90 @@ class Database:
             return None
         return int(fetched["min_star_count"])
 
-    def get_star_channel(self, guild_id:int) -> Union[int, NoneType]:
-        """ Gets the starboard channel ID """
+    def get_star_channel(self, guild_id: int) -> Union[int, NoneType]:
+        """Gets the starboard channel ID"""
         self.__db.execute(
             "SELECT star_channel FROM configuration WHERE guild_id = %s;", (guild_id,)
         )
         return self.__db.fetchone()["star_channel"]
 
-    def check_existing(self, message_id:int) -> Union[Star, None]:
-        """ Checks for existing star in the starboard """
+    def check_existing(self, _id: int) -> Union[Star, None]:
+        """Checks for existing star in the starboard"""
         self.__db.execute(
-            "SELECT * FROM stars WHERE message_id = %s;", (message_id,)
+            "SELECT * FROM stars WHERE star_id = %s OR message_id = %s;", (_id, _id)
         )
         fetched = self.__db.fetchone()
         if fetched is None:
             return None
-        return Star(fetched)
-    
-    def add_star(self, star_id:int, message_id:int, guild_id:int, author_id:int, star_count:int) -> NoneType:
-        """ Adds a star to the starboard """
+        return Star(fetched, _id)
+
+    def add_star(
+        self,
+        star_id: int,
+        message_id: int,
+        guild_id: int,
+        author_id: int,
+        star_count: int,
+    ) -> NoneType:
+        """Adds a star to the starboard"""
         self.__db.execute(
-            "INSERT INTO stars (star_id, message_id, guild_id, author_id, star_count) VALUES (%s, %s, %s, %s, %s);", (star_id, message_id, guild_id, author_id, star_count)
+            "INSERT INTO stars (star_id, message_id, guild_id, author_id, star_count) VALUES (%s, %s, %s, %s, %s);",
+            (star_id, message_id, guild_id, author_id, star_count),
         )
         return
 
-    def update_star(self, star_id:int, star_count:int) -> NoneType:
-        """ Updates the star count """
+    def update_reactors(
+        self, reactors: list, message_id: int, star_id: int
+    ) -> NoneType:
+        """Updates the reactors for a star"""
+        data = list((r, message_id, star_id) for r in reactors)
         self.__db.execute(
-            "UPDATE stars SET star_count = %s WHERE star_id = %s;", (star_count, star_id)
+            """DELETE FROM star_reactors WHERE star_id = %s AND message_id = %s;""", (star_id, message_id)
+        )
+        self.__db.executemany(
+            """INSERT INTO star_reactors (usr_id, message_id, star_id) VALUES (%s, %s, %s);""", data
         )
         return
 
-    def remove_star(self, star_id:int) -> NoneType:
-        """ Removes a star from the starboard """
+    def remove_reactor(self, reactor_id: int, _id: int) -> NoneType:
+        """Removes a reactor from the starboard"""
         self.__db.execute(
-            "DELETE FROM stars WHERE star_id = %s;", (star_id,)
+            "DELETE FROM star_reactors WHERE usr_id = %s AND star_id = %s OR message_id = %s;", (reactor_id, _id, _id)
         )
         return
+
+    def get_reactors(self, _id: int) -> list:
+        """Gets the reactors for a star"""
+        self.__db.execute(
+            "SELECT usr_id FROM star_reactors WHERE star_id = %s OR message_id = %s;",
+            (_id, _id),
+        )
+        reactors = self.__db.fetchall()
+        return [r["usr_id"] for r in reactors]
+
+    def update_star(self, star_id: int, star_count: int) -> NoneType:
+        """Updates the star count"""
+        self.__db.execute(
+            "UPDATE stars SET star_count = %s WHERE star_id = %s;",
+            (star_count, star_id),
+        )
+        return
+
+    def remove_star(self, star_id: int) -> NoneType:
+        """Removes a star from the starboard"""
+        self.__db.execute("DELETE FROM star_reactors WHERE star_id = %s;", (star_id,))
+        self.__db.execute("DELETE FROM stars WHERE star_id = %s;", (star_id,))
+        return
+
+    def get_stars(self, guild_id: int) -> list:
+        """Gets all the stars for a guild"""
+        self.__db.execute(
+            "SELECT * FROM stars WHERE guild_id = %s ORDER BY star_count DESC;", (guild_id,)
+        )
+        stars = self.__db.fetchall()
+        return [Star(s, s["star_id"]) for s in stars]
+        
+
 
 # here is the table structure for you self hosters
 
--- utils/models.py
+++ utils/models.py
@@ -1,7 +1,16 @@
 class Star:
-    def __init__(self, star_dict):
+    def __init__(self, star_dict:dict, msg_id:int):
+        self.__message_id = msg_id
         self.star_id = star_dict['star_id']
         self.message_id = star_dict['message_id']
         self.guild_id = star_dict['guild_id']
         self.author_id = star_dict['author_id']
         self.star_count = star_dict['star_count']
+        self.type = self.getType()
+        """ 0 = Starred Msg | 1 = Starboard Msg"""
+    
+    def getType(self):
+        if self.__message_id == self.message_id:
+            return 0
+        elif self.__message_id == self.star_id:
+            return 1
