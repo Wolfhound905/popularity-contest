cc5b4abe70ede235398d89e980ed3042bf49c4b4
Wolfhound905 <aiden8green@gmail.com>
2022-01-24 04:01:05 +0000

    going to do some public debugging support

--- commands/manage.py
+++ commands/manage.py
@@ -1,4 +1,4 @@
-from dis_snek import OptionTypes, slash_command, slash_option, InteractionContext, Scale
+from dis_snek import OptionTypes, slash_command, slash_option, InteractionContext, Scale, context_menu, CommandTypes
 
 from utils.database import Database
 
@@ -8,16 +8,16 @@ class ManageStars(Scale):
         self.bot = bot
         self.db: Database = self.bot.db
 
-    @slash_command("remove", "Remove a star")
-    @slash_option("star_id", "The star on the Starboard", OptionTypes.INTEGER, True)
-    @slash_option(
-        "blacklist",
-        "Prevent this message from being on the again?",
-        OptionTypes.BOOLEAN,
-        False,
-    )
-    async def remove(self, ctx: InteractionContext, star_id, blacklist):
-        await ctx.send("Coming soon™️", ephemeral=True)
+    # @slash_command("remove", "Remove a star")
+    # @slash_option("star_id", "The star on the Starboard", OptionTypes.INTEGER, True)
+    # @slash_option(
+    #     "blacklist",
+    #     "Prevent this message from being on the again?",
+    #     OptionTypes.BOOLEAN,
+    #     False,
+    # )
+    # async def remove(self, ctx: InteractionContext, star_id, blacklist):
+    #     await ctx.send("Coming soon™️", ephemeral=True)
         # try:
         #     if 64 > star_id.bit_length() < 22:
         #         raise ValueError("ID (snowflake) is not in correct discord format!")
@@ -28,6 +28,19 @@ class ManageStars(Scale):
         # if star is None:
         #     await ctx.reply(f"Star `{star_id}` not found")
         #     return
+
+    @context_menu("Debug Message", CommandTypes.MESSAGE)
+    async def debug_message(self, ctx: InteractionContext):
+        await ctx.defer(ephemeral=True)
+        existing = self.db.check_existing(ctx.target_id.id)
+        
+        if existing is None:
+            ...
+
+
+        await ctx.send("Coming soon.")
+    
+    
 
 
 def setup(bot):
